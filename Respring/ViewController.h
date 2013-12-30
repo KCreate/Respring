ec7d427d4aae8ad88743b591488d7eb81086b75c
Leonard Schuetz <leni.schuetz13@gmail.com>
2013-12-30 05:17:54 +0100

    Added features / Debugging

--- Respring/ViewController.h
+++ Respring/ViewController.h
@@ -8,6 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    IBOutlet UIImageView *background;
+    IBOutlet UIButton *trigger_respring;
+    IBOutlet UIView *trigger_respring_background;
+}
+
+@property UIImageView *background;
+@property UIButton *trigger_respring;
+@property UIView *trigger_respring_background;
+
+-(IBAction)respring:(id)sender;
 
 @end
--- Respring/ViewController.m
+++ Respring/ViewController.m
@@ -8,22 +8,60 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
+@synthesize background;
+@synthesize trigger_respring;
+@synthesize trigger_respring_background;
+
+-(BOOL)prefersStatusBarHidden{
+    return YES;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    //Configure the background
+    background.image = [UIImage imageNamed:@"wallpaper.png"];
+    background.alpha = 0;
+    
+    //Configure the button
+    trigger_respring.alpha = 0;
+    
+    //Configure the background of the button
+    trigger_respring_background.backgroundColor = [UIColor blackColor];
+    trigger_respring_background.alpha = 0;
+    
+    //Start an animation which shows the background and the button in a nice animation
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:0.6];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
+        background.alpha = 1;
+        trigger_respring.alpha = 1;
+        trigger_respring_background.alpha = 0.4;
+    [UIView commitAnimations];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(IBAction)respring:(id)sender {
+    //Kill the Springboard proccess. This causes a "respring"
+    
+    //Configure the AlertView
+    UIAlertView *alertview = [[UIAlertView alloc] initWithTitle:@"Respring"
+                                                        message:@"Are you sure you want to respring?"
+                                                       delegate:self cancelButtonTitle:@"No!"
+                                              otherButtonTitles:@"Yes!", nil];
+    [alertview show];
+}
+
+- (void)alertView:(UIAlertView *)choose_network_for_share clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 1) {
+        [self respring];
+    }
+}
+
+-(void)respring {
+    //Respring the device
+    system("killall SpringBoard");
 }
 
 @end
